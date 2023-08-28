c3785accebfe60b22a019734e7ba7086954b437f
Oleh Monchuk <olegio.monya@gmail.com>
2023-08-29 00:07:18 +0200

    Add styles to Products and add localStorage

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,22 +1,55 @@
 class Products { 
+    constructor() { 
+        this.classNameActive = "products-element__btn_active";
+        this.labelAdd = "Buy";
+        this.labelRemove = "Don't buy";
+    }
+
+    handleSetLocationStorage(element, id) {
+        const {pushProduct, products } = localStorageUtil.putProducts(id);
+        
+        if (pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else { 
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+     }
 
     render() { 
+
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
 
         CATALOG.forEach(({ id, title, price, img, autor }) => {
+            let activeClass = "";
+            let activeText = "";
+
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd;
+            } else { 
+                activeClass = " " + this.classNameActive;
+                activeText = this.labelRemove;
+            }
+
             htmlCatalog += `
-                <li>
-                    <img src="${img}" />
-                    <span>${title}</span>
-                    <span>${autor}</span>
-                    <span>${price}</span>
-                    <button>Add in basket </button>
+                <li class="products-element">
+                    <img class="products-element__img" src="${img}" />
+                    <span class="products-element__title">${title}</span>
+                    <span class="products-element__autor">${autor}</span>
+                    <span class="products-element__price">
+                       💵 ${price.toLocaleString()} грн
+                        </span>
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+                        ${activeText}
+                    </button>
                 </li>
             `;
         });
 
         const html = `
-            <ul>
+            <ul class="products-conteiner">
                 ${htmlCatalog}
             </ul>
         `;
